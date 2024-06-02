0379ff6ef3a21dc006f6e763cadc738ab2aabf51
rar34 <ra.cse34@gmail.com>
2024-06-03 00:59:21 +0600

    created auth provider and sign up and sign in method

--- src/main.jsx
+++ src/main.jsx
@@ -9,14 +9,17 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query'
+import AuthProvider from './providers/AuthProvider.jsx';
 
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,8 +1,8 @@
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import { createContext, useEffect, useState } from "react";
-import app from "../firebase/firebase.config";
 import PropTypes from 'prop-types'
 import useAxiosPublic from "../hooks/useAxiosPublic";
+import app from "../firebase/firebase.config";
 
 
 export const AuthContext = createContext(null)
