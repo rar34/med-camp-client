5231dd5fbf507f8336c72465d4e0edee5dd34d9b
rar34 <ra.cse34@gmail.com>
2024-06-10 19:18:47 +0600

    delete registered camp when unpaid from admin pannel

--- src/pages/Dashboard/OrganizerProfile/ManageRegCamp.jsx
+++ src/pages/Dashboard/OrganizerProfile/ManageRegCamp.jsx
@@ -1,6 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import Swal from "sweetalert2";
+import { GiConfirmed } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 
 const ManageRegCamp = () => {
     const axiosSecure = useAxiosSecure();
@@ -30,14 +32,39 @@ const ManageRegCamp = () => {
         }
     }
 
+    const handleCancel = (id) => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, Cancel it!"
+        }).then(async (result) => {
+            if (result.isConfirmed) {
+                const res = await axiosSecure.delete(`/regCamps/${id}`)
+                console.log(res.data)
+                if (res.data.deletedCount > 0) {
+                    Swal.fire({
+                        title: "Cancelled!",
+                        text: "Your Camp has been cancelled from registered camp.",
+                        icon: "success"
+                    });
+                    refetch();
+                }
+            }
+        });
+    }
+
 
     return (
         <div className="p-2 md:p-10 rounded-lg min-h-screen">
             <div className="overflow-x-auto">
                 <div className="space-y-3 mb-14 w-3/4 mx-auto">
-                    <h2 className="text-3xl text-[#6F42C1] text-center font-bold">Manage Camps</h2>
+                    <h2 className="text-3xl text-[#6F42C1] text-center font-bold">Manage Registered Camps</h2>
                     <hr />
-                    <p className="text-center text-gray-600">Welcome to the Manage Camps section. Here, you can view and manage all the camps in our system. Use the table below to see detailed information about each camp, including their current status and details. You can edit, update, or delete camp information as needed to keep everything up-to-date.</p>
+                    <p className="text-center text-gray-600">Welcome to the Manage Registered Camps section. Here, you can view and manage all the camps which are registered by participants. Used the table below to see detailed information about each camp, including their current payment status, confirmation status and details. You can update camp status to confirmed or cancel camp information as needed to keep everything up-to-date.</p>
                     <hr />
                 </div>
                 <table className="table">
@@ -78,7 +105,7 @@ const ManageRegCamp = () => {
                                 </td>
                                 <td>
                                     {
-                                        camp.paymentStatus === 'Paid' && camp.confirmStatus === 'Confirmed' ? <button disabled className="btn btn-xs text-white bg-green-600">Cancel</button> : <button className="btn btn-xs text-white bg-red-600">Cancel</button>
+                                        camp.paymentStatus === 'Paid' && camp.confirmStatus === 'Confirmed' ? <GiConfirmed className="text-3xl text-green-600" /> : <button onClick={()=>handleCancel(camp._id)} className="btn btn-xs text-white bg-red-600"><ImCross /></button>
                                     }
                                     
                                 </td>
